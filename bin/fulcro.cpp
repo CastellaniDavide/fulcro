d2bcf053e3246c1f1239ba7c46eec82fcbb9eb90
DavideC03 <davide.castellani.5.8.03@gmail.com>
2020-11-25 11:57:09 +0100

    90/100

--- bin/fulcro.cpp
+++ bin/fulcro.cpp
@@ -19,24 +19,69 @@
 using namespace std;
 
 // Variabiles
-int N;
+int M, temp1, temp2;
+
+vector <vector<int> > giocatori;
+
+int visita(int n)
+{
+    int counter = 0;
+    vector <bool> visitato (M + 1, false);
+
+    visitato[n] = true;
+    stack <int> pila;
+    pila.push(0);
+
+    while(!pila.empty())
+    {
+        int now_ = pila.top();
+        pila.pop();
+        if(!visitato[now_])
+        {
+            visitato[now_] = true;
+            for(int i = 0; i < giocatori[now_].size(); ++i)
+                pila.push(giocatori[now_][i]);
+        }
+    }
+
+    for(int i = 2; i < visitato.size(); ++i)
+        if(!visitato[i])
+            counter++;
+
+    return counter;
+}
 
 // Main code
 int main()
 {
-  // Cncomment the following lines if you want to read/write from files
-  // freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+    // Cncomment the following lines if you want to read/write from files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
-  // Input
-  cin >> N;
+    // Input
+    cin >> M;
+    giocatori.resize(M + 1);
+    for (int i = 0; i < M; ++i)
+    {
+        cin >> temp1 >> temp2;
+        giocatori[temp1 - 1].push_back(temp2 - 1);
+    }
 
-  // Code
-  // ...
+    int result;
+    int escluded_player = -1;
+    for(int i = 1; i < M; ++i)
+    {
+        int temp = visita(i);
+        if(temp > escluded_player)
+        {
+            escluded_player = temp;
+            result = i;
+        }
+    }
 
-  // Output
-  cout << N << endl;
+// Output
+    cout << result + 1 << endl;
 
-  // End
-  return 0;
+// End
+    return 0;
 }
